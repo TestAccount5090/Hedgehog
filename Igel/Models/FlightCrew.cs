be19e9b8b566ef6a8fbb453c4fbdf75c967392aa
Sergey <example@example.com>
2026-02-24 11:50:17 +0100

    Models are ready

--- Igel/Models/FlightCrew.cs
+++ Igel/Models/FlightCrew.cs
@@ -5,7 +5,7 @@
         public Pilot Captain {  get; set; }
         public Pilot? Copilot { get; set; }
         public string? SecondOfficer {  get; set; }
-        //public string? Navigator { get; set; }
-        //public string? FlightEngeneer { get; set; }
+        public string? Navigator { get; set; }
+        public string? FlightEngeneer { get; set; }
     }
 }
